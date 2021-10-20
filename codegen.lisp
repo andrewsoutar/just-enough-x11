e52bc5a34e32841e1f79e857969d030db0498c08
Andrew Soutar <andrew@andrewsoutar.com>
2021-10-20 14:17:35 -0400

    Refactor structure codegen

--- codegen.lisp
+++ codegen.lisp
@@ -18,60 +18,86 @@
           (temp #2=(string-downcase (subseq name start end)) (concatenate 'string temp "-" #2#)))
          ((null end) temp))))
 
+(ct (defun gen-setter (buffer-ptr-var width alignment &key signedp name-hint)
+      #.(format nil "Generates code for setting a (potentially ~
+      unaligned) WIDTH-byte int in the buffer. Returns values like ~
+      MAKE-STRUCT-OUTPUTTER.")
+      (do ((value-var (if name-hint (make-symbol name-hint) (gensym "VALUE")))
+           (type `(,(if signedp 'signed-byte 'unsigned-byte) ,(* 8 width)))
+           (initializers (make-collector))
+           (offset 0))
+          ((>= offset width) (values value-var type () width initializers alignment))
+        (let* ((worst-case (logior (car alignment) (cdr alignment) 8 #-64-bit 4))
+               (stride (- (logior (ash -1 (1- (integer-length (- width offset)))) worst-case (- worst-case))))
+               (type (find-symbol (format nil "UINT~A" (* 8 stride)) :keyword)))
+          (assert type)
+          (collect initializers
+            `(setf (mem-ref ,buffer-ptr-var ,type)
+                   (ldb (byte ,(* stride 8) ,(* 8 #+big-endian (- width offset stride) #+little-endian offset))
+                        ,value-var))
+            `(incf-pointer ,buffer-ptr-var ,stride))
+          (incf offset stride)
+          (incf (car alignment) stride)))))
+
 (ct
-  (defun make-struct-outputter (fields buffer-ptr &key request-hack (offset 0) (alignment (cons 0 1)))
-    "Returns values: lambda-list, length-form, declarations, initializers, alignment"
+  (defun make-struct-outputter (fields buffer-ptr-var &key request-hack (alignment (cons 0 1)))
+    "Returns values: lambda-list, types, wrapper-forms-collector, length-form, initializers-collector, alignment"
     (let ((lambda-list (make-collector))
-          (declarations (make-collector))
+          (types (make-collector))
+          (wrapper-forms (make-collector))
+          (length-forms (make-collector))
           (initializers (make-collector)))
       (when request-hack
         ;; In requests, the first byte is at offset 1
-        (incf offset)
+        (collect initializers `(incf-pointer ,buffer-ptr-var 1))
+        (collect length-forms 1)
         (incf (car alignment)))
-      (dolist (field fields
-                     (values (collect lambda-list) offset (collect declarations) (collect initializers) alignment))
-        (let (width)
-          (flet ((make-setter (val)
-                   (let* ((worst-case (logior (car alignment) (cdr alignment) width #-32-bit 8 #-64-bit 4))
-                          (stride (1+ (logandc1 worst-case (1- worst-case))))
-                          (type (find-symbol (format nil "UINT~A" (* 8 stride)) :keyword)))
-                     (assert type)
-                     (if (= stride width)
-                         `(setf (mem-ref ,buffer-ptr ,type ,offset) (ldb (byte ,stride 0) ,val))
-                         (let ((val-var (gensym "VAL")))
-                           `(let ((,val-var ,val))
-                              ,@(loop for i from 0 below width by stride
-                                      collect `(setf (mem-ref ,buffer-ptr ,type ,(+ offset i))
-                                                     (ldb (byte ,stride #+big-endian ,(- width stride i)
-                                                                        #+little-endian i)
-                                                          ,val-var)))))))))
-            (match-ecase field
-              ((:pad bytes &rest)
-               (setf width bytes))
-              ((:field &key name type &rest)
-               (let ((var (make-symbol (substitute #\- #\_ (string-upcase name)))))
-                 (collect lambda-list var)
+      (dolist (field fields)
+        (match-ecase field
+          ((:pad bytes &rest)
+           (collect initializers `(incf-pointer ,buffer-ptr-var ,bytes))
+           (collect length-forms bytes)
+           (incf (car alignment) bytes))
+          ((:field &key name type &rest)
+           (let ((name-hint (substitute #\- #\_ (string-upcase name)))
+                 override-type)
+             (multiple-value-bind (inner-ll inner-types inner-wrapper-forms inner-length-form
+                                   inner-initializers inner-alignment)
                  (match-ecase (find-type type)
-                   (nil (error "~A: undefined type ~A" name type))
-                   (:xid (setf width 4)
-                         (collect declarations `(type (unsigned-byte 29) ,var))
-                         (collect initializers (make-setter var)))
+                   (:xid (setf override-type '(unsigned-byte 29))
+                         (gen-setter buffer-ptr-var 4 alignment :name-hint name-hint))
                    (((m:and kind (m:or :unsigned :signed)) bits)
                     (multiple-value-bind (bytes leftover) (floor bits 8)
                       (assert (zerop leftover))
-                      (setf width bytes))
-                    (collect declarations `(type (,(if (eql kind :signed) 'signed-byte 'unsigned-byte) ,bits) ,var))
-                    (collect initializers (make-setter var)))
-                   (t (error "Unable to parse type: ~A" type)))))
-              (t (error "Unable to parse field: ~A" field))))
-          (when width
-            (when request-hack
-              ;; This field fits in the 1-byte gap in the request
-              ;; header. After that, continue at offset 4.
-              (assert (= width 1))
-              (setf width 3 request-hack nil))
-            (incf offset width)
-            (incf (car alignment) width)))))))
+                      (gen-setter buffer-ptr-var bytes alignment :signedp (eql kind :signed) :name-hint name-hint)))
+                   (type-form (error "Unable to parse type \"~A\" as ~A" type type-form)))
+               (match-ecase inner-ll
+                 (())
+                 ((m:and var (m:type symbol))
+                  (collect lambda-list var)
+                  (collect types (or override-type inner-types)))
+                 ((&rest inner-ll)
+                  (let ((var (make-symbol name-hint)))
+                    (collect lambda-list var)
+                    (collect types 'list)
+                    (collect wrapper-forms
+                      `(destructuring-bind (,@inner-ll) ,var
+                         (declare ,@(mapcar (lambda (tp v) `(type ,tp ,v)) inner-types inner-ll)))))))
+               (collect-all wrapper-forms inner-wrapper-forms)
+               (collect length-forms inner-length-form)
+               (collect-all initializers inner-initializers)
+               (setf alignment inner-alignment)))))
+        (when request-hack
+          ;; This field fits in the 1-byte gap in the request
+          ;; header. After that, continue at offset 4.
+          (assert (equal (apply #'+ (collect length-forms)) 2))
+          (collect length-forms 2)
+          (collect initializers `(incf-pointer ,buffer-ptr-var 2))
+          (incf (car alignment) 2)
+          (setf request-hack nil)))
+
+      (values (collect lambda-list) (collect types) wrapper-forms
+              `(+ ,@(collect length-forms)) initializers alignment))))
 
 (ct
   (defparameter *supported-messages*
@@ -112,13 +138,17 @@
            (collect forms
              (let ((connection (copy-symbol 'connection))
                    (length (gensym "LENGTH"))
-                   (buffer (gensym "BUFFER")))
-               (multiple-value-bind (lambda-list length-form decls initializers)
-                   (make-struct-outputter fields buffer :request-hack t :alignment (cons 0 0))
+                   (buffer (gensym "BUFFER"))
+                   (buffer-fill (gensym "BUFFER-FILL")))
+               (multiple-value-bind (lambda-list types wrapper-forms length-form initializers)
+                   (make-struct-outputter fields buffer-fill :request-hack t :alignment (cons 0 0))
                  `(defun ,name (,connection ,@lambda-list)
                     (declare (type foreign-pointer ,connection)
-                             ,@decls)
-                    (let ((,length ,length-form))
-                      (with-pointer-to-bytes (,buffer ,length)
-                        ,@initializers
-                        (xcb-send ,connection ,opcode nil ,buffer ,length (null-pointer) 0)))))))))))))
+                             ,@(mapcar (lambda (tp v) `(type ,tp ,v)) types lambda-list))
+                    (nest ,@(collect wrapper-forms)
+                      (let ((,length ,length-form))
+                        (with-pointer-to-bytes (,buffer ,length)
+                          (let ((,buffer-fill ,buffer))
+                            ,@(collect initializers)
+                            (assert (pointer-eq (inc-pointer ,buffer ,length) ,buffer-fill)))
+                          (xcb-send ,connection ,opcode nil ,buffer ,length (null-pointer) 0))))))))))))))
